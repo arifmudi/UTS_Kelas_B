20fcad57e1ac1cd5dac880c80efdf21979b0a9e1
almanazizi <148742306+almanazizi@users.noreply.github.com>
2023-11-07 07:37:49 +0700

    if-statements.php

--- 2255201001_UTS/conditionals_and_logic_php/Booleans and Comparison Operators/if-statements.php
+++ 2255201001_UTS/conditionals_and_logic_php/Booleans and Comparison Operators/if-statements.php
@@ -1,6 +1,30 @@
 <?php
 //Alman Azizi
-$markAnswer = FALSE
+namespace Codecademy;
+
+function chooseCheckoutLane($items)
+{
+    if ($items <= 12) {
+        return "express lane";
+    } else {
+        return "regular lane";
+    }
+}
 //2255201001
-function markAnswer($is_correct)
+function canIVote($age)
+{
+    if ($age >= 18) {
+        return "yes";
+    } else {
+        return "no";
+    }
+}
+
+echo chooseCheckoutLane(1);
+echo "\n\n";
+echo chooseCheckoutLane(13);
+echo "\n\n";
+echo canIVote(19);
+echo "\n\n";
+echo canIVote(13);
 //Kelas B
